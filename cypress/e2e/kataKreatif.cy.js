10765c22a22281c8baa847f893c76afc45e6e865
Daneeels <sasongkoindra76@gmail.com>
2026-05-25 09:18:52 +0700

    [ADD] Testing Kata Kreatif Page

---
+++ cypress/e2e/kataKreatif.cy.js
@@ -0,0 +1,16 @@
+describe('Kata Kreatif Testing', () => {
+
+    beforeEach('Visit Site', () => {
+        cy.visit('https://hub.ekraf.go.id/')
+        cy.url().should('include', 'hub.ekraf.go.id')
+        cy.contains('a', 'Kata Kreatif').click()
+        cy.url().should('include', '/kata-kreatif')
+    })
+
+    it('Cards Content Testing', () => {
+        cy.get('.kata-card.kata-card--green.default').should('contain.text', 'Kota/Kabupaten Kreatif yang Telah Mendaftar')
+        cy.get('.kata-card.kata-card--orange.default').should('contain.text', 'Kota/Kabupaten Kreatif yang Telah ditetapkan')
+    })
+
+    
+})
